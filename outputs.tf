270709dfb2634226d095e592a4a75c26e5603fc5
AS <37623164+AzySir@users.noreply.github.com>
2025-07-10 22:42:54 +0100

    Feat: Outputs Added for subnets

---
+++ outputs.tf
@@ -0,0 +1,12 @@
+output "public_subnets" {
+    value = { for key, subnet in aws_subnet.public : key => subnet.id }
+}
+
+output "private_subnets" {
+    value = { for key, subnet in aws_subnet.private : key => subnet.id }
+}
+
+output "database_subnets" {
+    value = { for key, subnet in aws_subnet.database : key => subnet.id }
+}
+
